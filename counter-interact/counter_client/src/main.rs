3033ec0feb488ee21d4b5c6ffd27da7b8fd7784f
Derked <nazek8@gmail.com>
2024-01-11 23:47:17 -0500

    final refactor client

--- counter-interact/counter_client/src/main.rs
+++ counter-interact/counter_client/src/main.rs
@@ -1,7 +1,7 @@
 use anchor_counter_interface::{
     increment_ix_with_program_id, initialize_ix_with_program_id, IncrementKeys, InitializeKeys,
 };
-use anyhow::{anyhow, Result};
+use anyhow::anyhow;
 use solana_client::rpc_client::RpcClient;
 use solana_program::pubkey::Pubkey;
 use solana_program::system_program;
@@ -12,8 +12,8 @@ use solana_sdk::transaction::Transaction;
 use std::path::{self, Path};
 use std::str::FromStr;
 
-fn main() -> Result<()> {
-    //Set these to your correct values
+fn main() -> anyhow::Result<()> {
+    /*SET THESE TO YOUR VALUES OR READ FROM*/
     let path_to_keypair = path::Path::new("/home/derked/.config/solana/id.json");
     let program_id = "CQ2VvuR8Du2WQq1XWmzBKxmK4arc7BVReWMxUMW3nJs5";
     let rpc_url = "http://localhost:8899";
@@ -24,38 +24,45 @@ fn main() -> Result<()> {
         program_id,
     } = SetUpClient::new(rpc_url, &path_to_keypair, program_id)?;
 
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 2 {
+        Err(anyhow!("Please specify an action: init, incr, init_incr"))?;
+    }
+
     let (counter_pda, _) = Pubkey::find_program_address(&[b"counter"], &program_id);
 
-    let mut sig = initialize(&rpc, &counter_pda, &user, program_id);
+    let sig = match args[1].as_str() {
+        "init" => initialize(&rpc, &counter_pda, &user, program_id),
+        "incr" => increment(&rpc, &counter_pda, &user, program_id),
+        "init_incr" => initialize_and_increment(&rpc, &counter_pda, &user, program_id),
+        _ => Err(anyhow!("Please specify an action: init, incr, init_incr"))?,
+    };
 
-    match sig {
-        Ok(sig) => println!("Initialize Tx Successful with Signature: {:?}", sig),
-        Err(e) => {
+    match (sig, args[1].as_str()) {
+        (Ok(sig), _) => println!("Tx Successful with Signature: {:?}", sig),
+        (Err(e), "init") => {
             if e.to_string().contains("custom program error: 0x0") {
-                println!("Counter Account Already Initialized ... continuing");
+                println!("Counter Account Already Initialized!");
+            } else {
+                Err(anyhow!("Something went wrong with initializing: {:?}", e))?;
+            }
+        }
+        (Err(e), "incr") => Err(anyhow!("Something went wrong with incrementing: {:?}", e))?,
+        (Err(e), "init_incr") => {
+            if e.to_string().contains("custom program error: 0x0") {
+                println!("Counter Account Already Initialized! Cannot increment afterwards!");
             } else {
                 Err(anyhow!(
-                    "Something went wrong initializing the counter account: {:?}",
+                    "Something went wrong with initializing or incrementing: {:?}",
                     e
                 ))?;
             }
         }
+        _ => unreachable!(),
     }
 
     display_counter_info(&rpc, &counter_pda)?;
-
-    sig = increment(&rpc, &counter_pda, &user, program_id);
-
-    match sig {
-        Ok(sig) => println!("Increment Tx Successful with Signature: {:?}", sig),
-        Err(e) => Err(anyhow!(
-            "Something went wrong incrementing the counter account: {:?}",
-            e
-        ))?,
-    }
-
-    display_counter_info(&rpc, &counter_pda)?;
-
     Ok(())
 }
 
@@ -88,11 +95,7 @@ fn initialize(
     user: &Keypair,
     program_id: Pubkey,
 ) -> anyhow::Result<Signature> {
-    let init_keys = InitializeKeys::from([
-        *counter_pda,       //counter
-        user.pubkey(),      //user
-        system_program::ID, //system program
-    ]);
+    let init_keys = InitializeKeys::from([*counter_pda, user.pubkey(), system_program::ID]);
 
     let ix = initialize_ix_with_program_id(program_id, init_keys)?;
     let recent_blockhash = rpc.get_latest_blockhash()?;
@@ -133,5 +136,31 @@ fn increment(
 
     let sig = rpc.send_and_confirm_transaction(&tx)?;
 
+    Ok(sig)
+}
+
+///Initialize the Counter Account and Increment
+///Will not increment if the Counter Account is already initialized
+fn initialize_and_increment(
+    rpc: &RpcClient,
+    counter_pda: &Pubkey,
+    user: &Keypair,
+    program_id: Pubkey,
+) -> anyhow::Result<Signature> {
+    let init_keys = InitializeKeys::from([*counter_pda, user.pubkey(), system_program::ID]);
+    let init_ix = initialize_ix_with_program_id(program_id, init_keys)?;
+
+    let increment_keys = IncrementKeys::from([*counter_pda, user.pubkey()]);
+    let increment_ix = increment_ix_with_program_id(program_id, increment_keys)?;
+
+    let recent_blockhash = rpc.get_latest_blockhash()?;
+    let tx = Transaction::new_signed_with_payer(
+        &[init_ix, increment_ix],
+        Some(&user.pubkey()),
+        &[&user],
+        recent_blockhash,
+    );
+
+    let sig = rpc.send_and_confirm_transaction(&tx)?;
     Ok(sig)
 }
